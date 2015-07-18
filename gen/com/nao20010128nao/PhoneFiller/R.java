1071f4c4717acfeb122a70b6f0df5ae9a402afef
nao20010128@gmail.com <nao20010128@gmail.com>
2015-07-19 02:08:33 +0900

    UI関係

--- gen/com/nao20010128nao/PhoneFiller/R.java
+++ gen/com/nao20010128nao/PhoneFiller/R.java
@@ -17,10 +17,14 @@ public final class R {
         public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int app_name=0x7f040000;
+        public static final int intsize=0x7f040001;
+        public static final int sdsize=0x7f040002;
+        public static final int startfill=0x7f040003;
+        public static final int stopfill=0x7f040004;
     }
     public static final class style {
+        public static final int AppDialog=0x7f050001;
         public static final int AppTheme=0x7f050000;
     }
 }
